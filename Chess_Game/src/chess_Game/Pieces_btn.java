682e3c6227c4a82fa6f755cf01d99ca03c3ee7f4
Rohan Karmakar <rohankarmakar97@gmail.com>
2018-09-10 15:47:06 +0530

    Add files via upload

---
+++ Chess_Game/src/chess_Game/Pieces_btn.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Rahul on 10-Sep-2018
+ * 
+ */
+package chess_Game;
+
+import javax.swing.JButton;
+
+public class Pieces_btn extends JButton {
+	public Pieces_btn()
+	{
+		JButton btn = new JButton();
+		
+	}
+}
---
+++ Chess_Game/src/chess_Game/Type.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Rahul on 10-Sep-2018
+ * 
+ */
+
+/**
+ * @author Payel
+ *
+ */
+package chess_Game;
+
+
+public enum Type
+{
+	King,Queen,Bishop,Knight,Rook,Pawn
+}
