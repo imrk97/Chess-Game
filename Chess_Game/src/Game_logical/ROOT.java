5409b654783a4b7009291df9a612836a8b620bf0
rohan <rohan@rohan-Inspiron-3543>
2018-09-17 07:44:49 +0530

    ROOT creation

---
+++ Chess_Game/src/Game_logical/ROOT.java
@@ -0,0 +1,11 @@
+package Game_logical;
+/**
+ * Created by Rohan on 10-Sep-2018*/
+import chess_Game.Type;
+public class ROOT extends  Piece {
+	Type type;
+	ROOT (int x,int y , Player player){
+		super(x,y,player);
+		
+	}
+}
