61b487e00dae881bdd49490f9038d9107e8a9fb5
rohan <rohan@rohan-Inspiron-3543>
2018-09-17 08:25:41 +0530

    ROOK creation

---
+++ Chess_Game/src/Game_logical/ROOK.java
@@ -0,0 +1,14 @@
+//created by imrk97
+package Game_logical;
+import chess_Game.*;
+public class ROOK extends Piece {
+	Type type;
+	ROOK(int x,int y,Player player){
+		super(x,y,player);
+		type=Type.Rook;
+		
+	}
+	boolean isValidPath(int dest_x,int dest_y) {
+		
+	}
+}
